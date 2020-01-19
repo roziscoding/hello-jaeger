7519e3e8a2ca4d289d31f6ca10da9261a5a7c8d5
Rogério Munhoz <rogerio.j.munhoz@gmail.com>
2020-01-19 01:06:28 -0300

    Adiciona código que realiza rastreamento de forma automática

--- service-2/index.js
+++ service-2/index.js
@@ -1,3 +1,26 @@
+const { NodeTracer } = require('@opentelemetry/node')
+const { initGlobalTracer } = require('@opentelemetry/core')
+const { SimpleSpanProcessor } = require('@opentelemetry/tracing')
+const { JaegerExporter } = require('@opentelemetry/exporter-jaeger')
+
+const tracer = new NodeTracer({
+  plugins: {
+      http: {
+        enabled: true,
+        path: '@opentelemetry/plugin-http'
+      }
+  }
+})
+
+const exporter = new JaegerExporter({
+  serviceName: 'service-2',
+  host: 'localhost'
+})
+
+tracer.addSpanProcessor(new SimpleSpanProcessor(exporter))
+
+initGlobalTracer(tracer)
+
 const axios = require('axios')
 const express = require('express')
 
